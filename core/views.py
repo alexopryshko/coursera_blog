6afbb69ad4e3a48257ec458ffa659dccad79a7ec
Alex Opryshko <alexopryshko@yandex.ru>
2018-01-04 22:29:08 +0300

    3.4.5

--- core/views.py
+++ core/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('ok')
+    return render(request, 'core/index.html')
--- coursera_blog/settings.py
+++ coursera_blog/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'coursera_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
