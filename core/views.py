5fe24a110f92072a1477da7dbe5de848e9d0d78d
Alex Opryshko <alexopryshko@yandex.ru>
2018-01-14 11:51:23 +0300

    3.4.5

--- core/views.py
+++ core/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+
+def topic_details(request, pk):
+    return render(request, 'core/topic_details.html')
--- coursera_blog/urls.py
+++ coursera_blog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from core.views import index
+from core.views import index, topic_details
 
 urlpatterns = [
     url(r'^index/$', index),
+    url(r'^topic/(?P<pk>\d+)/$', topic_details, name='topic_details'),
     url(r'^admin/', admin.site.urls),
 ]
